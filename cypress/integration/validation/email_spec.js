4b0efe202794ffc8e5ed6b5254bf6cd8780633f1
strivegfx <notifydevon@gmail.com>
2018-09-02 14:22:44 +1200

    create email validation assertions

---
+++ cypress/integration/validation/email_spec.js
@@ -0,0 +1,31 @@
+const SUBMIT_NODE = 'button[type="submit"]';
+const EMAIL_NODE = 'input[name="Email"]';
+const INVALID_MESSAGE = "A valid Email address is required";
+
+describe("email field validation", () => {
+  beforeEach(() => {
+    cy.visit("/contact");
+  });
+
+  it("should be invalid if submitted with no email value", () => {
+    cy.get(SUBMIT_NODE).click();
+
+    cy.contains(INVALID_MESSAGE);
+  });
+
+  it("should be invalid if submitted with an incorrect email format", () => {
+    cy.get(EMAIL_NODE).type("test.com");
+
+    cy.get(SUBMIT_NODE).click();
+
+    cy.contains(INVALID_MESSAGE);
+  });
+
+  it("should be valid if submitted with a correct email format", () => {
+    cy.get(EMAIL_NODE).type("test@test.com");
+
+    cy.get(SUBMIT_NODE).click();
+
+    cy.contains(INVALID_MESSAGE).should("not.exist");
+  });
+});
