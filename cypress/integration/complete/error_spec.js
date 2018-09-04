ca0d75006e870cb34dc63a6c0bbcb6ef014fd444
strivegfx <notifydevon@gmail.com>
2018-09-04 20:42:17 +1200

    setup screenshots and video hooks

--- cypress/integration/complete/error_spec.js
+++ cypress/integration/complete/error_spec.js
@@ -1,3 +1,14 @@
+before(function() {
+  // We are stubbing out the network request to simulate a failure to POST the
+  // users message to the server. Because Cypress only uses XHR and not Fetch
+  // we need to turn off the Fetch feature in the browser to that the polyfill
+  // falls back to use XHR. This lets us hijack the route with Cypress =)
+  // NOTE: Use `Cypress` instead of `cy` so this persists across all tests.
+  Cypress.on("window:before:load", win => {
+    win.fetch = null;
+  });
+});
+
 describe("unsuccessful completion", function() {
   it("inform the users that their message has not been sent successfully", function() {
     cy.server({
--- cypress/support/commands.js
+++
@@ -1,8 +0,0 @@
-// We are stubbing out the network request to simulate a failure to POST the
-// users message to the server. Because Cypress only uses XHR and not Fetch
-// we need to turn off the Fetch feature in the browser to that the polyfill
-// falls back to use XHR. This lets us hijack the route with Cypress =)
-// NOTE: Use `Cypress` instead of `cy` so this persists across all tests.
-Cypress.on("window:before:load", win => {
-  win.fetch = null;
-});
